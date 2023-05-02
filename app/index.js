c1faa91866cf6ceb51804229216dff8f1af108e1
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-02 23:39:26 +0200

    MAJ

--- app/index.js
+++ app/index.js
@@ -10,119 +10,105 @@ v1.0
 April 2023
 `);
 
-/*=====================================================================*/
-const typeArticle = Object.freeze({
-  CAT_HTML: 1,
-  CAT_CSS: 2,
-  CAT_JAVASCRIPT: 3,
-  CAT_TOUS: 1,
-});
-
-const header = createMarkup("div", null, document.body, [
-  { name: "class", value: "container bg-success" },
-]);
-
-const header_row0 = createMarkup("div", null, header, [
-  { name: "class", value: "row" },
-]);
-
-const header_title = createMarkup(
-  "h1",
-  "Choissisez votre langage préféré...Et bonne lecture!",
-  header_row0,
-  [{ name: "class", value: "col h1 text-center mt-5" }]
-);
-
-const header_row1 = createMarkup("div", null, header, [
-  { name: "class", value: "row m-5" },
-]);
-
-const filters = ["Tous", "Ctml", "Css", "Js"];
-
-filters.forEach(function (filter) {
-  btn = createMarkup("button", filter, header_row1, [
-    {
-      name: "class",
-      value: "col btn btn-secondary shadow-5 m-5 p-2 active",
-    },
-  ]);
-});
-
-const main = createMarkup("div", null, document.body, [
-  { name: "class", value: "container bg-warning" },
-]);
-
-const main_row0 = createMarkup("div", null, main, [
-  { name: "class", value: "row text-left" },
-]);
-
-fetch("articles.json")
-  .then((response) => response.json())
-  .then((jsonResponse) => {
-    let parsedObjectsNumber = 0;
-
-    for (let readObject of jsonResponse) {
-      let rowInProgress = 0;
-      let article = 0;
-      switch (readObject.categorie) {
-        case typeArticle.CAT_HTML:
-          article = createMarkup("article", null, main_row0, [
-            {
-              name: "class",
-              value: "col-3 bg-info rounded shadow p-5",
-            },
-          ]);
-          break;
-        case typeArticle.CAT_CSS:
-          article = createMarkup("article", null, main_row0, [
-            {
-              name: "class",
-              value: "col-3 bg-warning rounded shadow p-5",
-            },
-          ]);
-          break;
-        case typeArticle.CAT_JAVASCRIPT:
-          article = createMarkup("article", null, main_row0, [
-            {
-              name: "class",
-              value: "col-3 bg-success rounded shadow p-5",
-            },
-          ]);
-          break;
-        default:
-          throw Error("Catégorie de l'article non reconnue");
-      }
-
-      let article_titre = createMarkup("h1", readObject.titre, article, [
-        {
-          name: "class",
-          value: `text-left h4 `,
-        },
-      ]);
-
-      const article_content = createMarkup("p", readObject.contenu, article, [
-        {
-          name: "class",
-          value: `text-left mt-5`,
-        },
-      ]);
-    }
-  });
-
-//   if (parsedObjectsNumber !== 16)
-//     throw new Error(
-//       "Le nombre d'articles contenus dans le fichier JSON est inccorect");
+displayStatic_elements();
+displayDynamic_elements();
 
 /*Déclaration des fonctions*/
-/**
- * Crée un élément du dom, lui ajoute du texte, le place comme dernier
- * enfant de parent et ajoute un attribut en utilisant le paramètre attribute
- * @param {String} markup_name
- * @param {String} text
- * @param {domElement} parent
- * @param {Array} attributes  (doit comprendre les propriétés name et value)
- * @returns domElement
- */
+function displayStatic_elements() {
+  const header = createMarkup("div", null, document.body, [
+    { name: "class", value: "container" },
+  ]);
+
+  const header_row0 = createMarkup("div", null, header, [
+    { name: "class", value: "row" },
+  ]);
+
+  const header_title = createMarkup(
+    "h1",
+    "Choissisez votre langage préféré...Et bonne lecture!",
+    header_row0,
+    [{ name: "class", value: "col h1 text-center mt-5" }]
+  );
+
+  const header_row1 = createMarkup("div", null, header, [
+    { name: "class", value: "row m-5" },
+  ]);
+
+  const filters = ["Tous", "Ctml", "Css", "Js"];
+
+  filters.forEach(function (filter) {
+    btn = createMarkup("button", filter, header_row1, [
+      {
+        name: "class",
+        value: "col btn btn-secondary shadow-5 m-5 p-2 active",
+      },
+    ]);
+  });
+}
+
+function displayDynamic_elements(filter = 0) {
+  const main = createMarkup("div", null, document.body, [
+    { name: "class", value: "container" },
+  ]);
+
+  const main_row0 = createMarkup("div", null, main, [
+    { name: "class", value: "row text-left" },
+  ]);
+
+  fetch("articles.json")
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      let parsedObjectsNumber = 0;
+
+      for (let readObject of jsonResponse) {
+        let rowInProgress = 0;
+        let article = 0;
+        switch (readObject.categorie) {
+          case typeArticle.CAT_HTML:
+            article = createMarkup("article", null, main_row0, [
+              {
+                name: "class",
+                value: "col-3 bg-info rounded shadow p-5 border border-dark",
+              },
+            ]);
+            break;
+          case typeArticle.CAT_CSS:
+            article = createMarkup("article", null, main_row0, [
+              {
+                name: "class",
+                value: "col-3 bg-warning rounded shadow p-5 border border-dark",
+              },
+            ]);
+            break;
+          case typeArticle.CAT_JAVASCRIPT:
+            article = createMarkup("article", null, main_row0, [
+              {
+                name: "class",
+                value: "col-3 bg-success rounded shadow p-5 border border-dark",
+              },
+            ]);
+            break;
+          default:
+            throw Error("Catégorie de l'article non reconnue");
+        }
+
+        let article_titre = createMarkup("h1", readObject.titre, article, [
+          {
+            name: "class",
+            value: `text-left h4 `,
+          },
+        ]);
+
+        const article_content = createMarkup("p", readObject.contenu, article, [
+          {
+            name: "class",
+            value: `text-left mt-5`,
+          },
+        ]);
+      }
+    });
+}
+
 function createMarkup(markup_name, text, parent, attributes = []) {
   const markup = document.createElement(markup_name);
   markup.textContent = text;
@@ -139,3 +125,11 @@ function createMarkup(markup_name, text, parent, attributes = []) {
 
   return markup;
 }
+
+/*enum =====================================================================*/
+const typeArticle = Object.freeze({
+  CAT_HTML: 1,
+  CAT_CSS: 2,
+  CAT_JAVASCRIPT: 3,
+  CAT_TOUS: 1,
+});
