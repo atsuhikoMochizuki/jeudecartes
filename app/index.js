efa28988f3b10d64df7ebe952f03a780ad629f0a
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-05-03 01:55:19 +0200

    MAJ

--- app/index.js
+++ app/index.js
@@ -18,9 +18,23 @@ April 2023
 let anchorMain = displayStatic_elements();
 
 displayDynamic_elements(anchorMain);
+
+const btn_Tous = document.querySelector(".Tous");
+btn_Tous.addEventListener("click", filtrer_Tous);
+
+const btn_Html = document.querySelector(".Html");
+btn_Html.addEventListener("click", filtrer_html);
+
+const btn_Css = document.querySelector(".Css");
+btn_Css.addEventListener("click", filtrer_css);
+
+const btn_Js = document.querySelector(".Js");
+btn_Js.addEventListener("click", filtrer_js);
+
 /*========================================================================*/
 
 /*Déclaration des fonctions*/
+/*========================================================================*/
 function displayStatic_elements() {
   const header = createMarkup("div", null, document.body, [
     { name: "class", value: "container" },
@@ -41,20 +55,25 @@ function displayStatic_elements() {
     { name: "class", value: "row m-5" },
   ]);
 
-  const filters = ["Tous", "Ctml", "Css", "Js"];
-
-  filters.forEach(function (filter) {
-    btn = createMarkup("button", filter, header_row1, [
-      {
-        name: "class",
-        value: "col btn btn-secondary shadow-5 m-5 p-2 active",
-      },
-    ]);
-  });
+  createFilteredButton("Tous", header_row1);
+  createFilteredButton("Html", header_row1);
+  createFilteredButton("Css", header_row1);
+  createFilteredButton("Js", header_row1);
 
   let mainAnchor = createMarkup("main", null, document.body, [
     { name: "class", value: "container" },
   ]);
+
+  let footer = createMarkup("footer", null, document.body, [
+    { name: "class", value: "container mt-5" },
+  ]);
+
+  let mentions = createMarkup(
+    "p",
+    "@Atsuhiko Mochizuki - Avril 2023 - for Diginamic formations",
+    footer,
+    [{ name: "class", value: "h6 text-center mt-5  mb-5 " }]
+  );
 
   return mainAnchor;
 }
@@ -151,6 +170,38 @@ function displayDynamic_elements(anchor) {
     });
 }
 
+function createFilteredButton(name, parent) {
+  let btnFilteredContainer = createMarkup("div", null, parent, [
+    {
+      name: "class",
+      value: "col m-5",
+    },
+  ]);
+
+  let btnFilter = createMarkup("div", null, btnFilteredContainer, [
+    {
+      name: "class",
+      value: name,
+    },
+  ]);
+
+  let divForInsertAndAlignButton = createMarkup("div", null, btnFilter, [
+    {
+      name: "class",
+      value: "row",
+    },
+  ]);
+
+  let btn = createMarkup("button", name, divForInsertAndAlignButton, [
+    {
+      name: "class",
+      value: "col  btn btn-secondary rounded shadow-5 border border-dark  m-3",
+    },
+  ]);
+
+  return btn;
+}
+
 function createMarkup(markup_name, text, parent, attributes = []) {
   const markup = document.createElement(markup_name);
   markup.textContent = text;
@@ -175,3 +226,17 @@ const typeArticle = Object.freeze({
   CAT_JAVASCRIPT: 3,
   CAT_TOUS: 1,
 });
+
+/*Fonctions callback affectées au boutons de l'interface*/
+function filtrer_css() {
+  console.log("Bonjour css");
+}
+function filtrer_Tous() {
+  console.log("Bonjour tous");
+}
+function filtrer_html() {
+  console.log("Bonjour html");
+}
+function filtrer_js() {
+  console.log("Bonjour js");
+}
